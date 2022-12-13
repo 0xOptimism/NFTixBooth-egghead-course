583a47570d0eed2d184896f94ba458c53a9128ca
Yannis <yannis182@outlook.fr>
2022-12-13 10:10:25 +0100

    Add an Image to an NFT with Token Metadata

--- packages/hardhat/contracts/NFTixBooth.sol
+++ packages/hardhat/contracts/NFTixBooth.sol
@@ -1,8 +1,10 @@
 pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "hardhat/console.sol";
+import "./Base64.sol";
 
 contract NFTixBooth is ERC721URIStorage {
     using Counters for Counters.Counter;
@@ -21,7 +23,40 @@ contract NFTixBooth is ERC721URIStorage {
 
     function mint() public {
         require(availableTickets > 0, "Not enought Tickets");
+
+        string[3] memory svg;
+        svg[
+            0
+        ] = '<svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"><text y="50">';
+        svg[1] = Strings.toString(currentId.current());
+        svg[2] = "</text></svg>";
+
+        string memory image = string(abi.encodePacked(svg[0], svg[1], svg[2]));
+        string memory encodedImage = Base64.encode(bytes(image));
+        console.log(encodedImage);
+
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{ "name": "NFTix #',
+                        Strings.toString(currentId.current()),
+                        '", "description": "A NFT-powered ticketing system", ',
+                        '"traits": [{ "trait_type": "Checked In", "value": "true" }, { "trait_type": "Purchased", "value": "true" }], ',
+                        '"image": "ipfs://QmTHNkB3VwuAd7cLF1vVzumX18osfwLLHiLg6QS36kgXPc" }'
+                    )
+                )
+            )
+        );
+
+        string memory tokenURI = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
+
+        console.log(tokenURI);
+
         _safeMint(msg.sender, currentId.current());
+        _setTokenURI(currentId.current(), tokenURI);
 
         currentId.increment();
         availableTickets = availableTickets - 1;
